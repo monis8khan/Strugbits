33eb4f961b39ff54fb48d5dd0cd03044e09b56e7
Monis Hasan Khan <monis8khan@gmail.com>
2021-03-04 23:23:28 +0500

    updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
+
+//Express App Initialization
 let app = express();
+//Mongo DB URI
+const dbUri = 'mongodb+srv://monis8khan:sr20dett@testing.bvbcz.mongodb.net/Employee?retryWrites=true&w=majority'
+//Mongoose App initialize
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology:true})
+.then(()=>{	//When database is connected with mongoose
+console.log('Database is connected')
+//Where our app is running
+let port = 8080;
+app.listen(port, ()=>{
+ console.log('The app is running on port ', port);
+})
+})
+.catch(err=>{console.log(err)});
 
+//EndPoints of our Application
 app.get('/', (req, res) => {
   res.send('Hello Abeera Monis!')
 })
 
-app.listen(3030, ()=>{
- console.log('The app is running on port 3030');
-})
+
 
