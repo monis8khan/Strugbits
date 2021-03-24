52c211183fb36e2d820aba6082d22a7499584d2d
Monis Hasan Khan <monis8khan@gmail.com>
2021-03-24 23:20:32 +0500

    body parser added

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,12 @@
-var express = require('express');
+var router = require('express').Router;
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
-var Employee = require('./models/employee')
-var api = require('./routes/auth-routes');
+var Employee = require('../models/employee')
 
-//Express App Initialization
-let app = express();
 
 //middleware Urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 
+/*
 //Mongo DB URI
 const dbUri = 'mongodb+srv://monis8khan:sr20dett@testing.bvbcz.mongodb.net/Employee?retryWrites=true&w=majority'
 //Mongoose App initialize
@@ -23,15 +20,15 @@ app.listen(port, ()=>{
 })
 })
 .catch(err=>{console.log(err)});
-
+*/
 //EndPoints of our Application
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send(`Hello This is my first api, routes are Type=GET : /employeeList, /employee`);
 })
 
-
+/*
 //API FOR NEW EMPLOYEE
-app.get('/newEmployee', (req, res)=>{
+router.get('/newEmployee', (req, res)=>{
 	var newEmployee = new Employee({
 		'employeeId':'001',
 		'firstName': 'Monis',
@@ -45,21 +42,20 @@ app.get('/newEmployee', (req, res)=>{
 			res.send(result)
 		}).catch(err=>{console.log(err)})
 })
-
+*/
 //API TO LIST ALL THE EMPLOYEES
-app.get('/employeeList', (req, res)=>{
-	Employee.find({}).then((results)=>{
-	res.send(results);
-	}).catch(err=>{console.log(err)});
-})
-
+//router.get('/employeeList', (req, res)=>{
+//	Employee.find({}).then((results)=>{
+//	res.send(results);
+//	}).catch(err=>{console.log(err)});
+//})
+/*
 //API TO LIST SINGLE EMPLOYEE
 app.get('/employee', (req, res)=>{
 	Employee.findById('6044b2f24f36312c45c00b2b').then((result)=>{
 	res.send(result);
 	}).catch(err=>{console.log(err)});
 })
-
 
 
 //POST API FOR NEW EMPLOYEE
@@ -89,6 +85,9 @@ app.post('/employee', async (req,res)=>{
 	});
 	res.send(response);
 })
-
+*/
 //route for login, exported from authRoute
-app.use('/api', api);
+//app.use('/api', authRoutes);
+
+
+module.exports = router;
